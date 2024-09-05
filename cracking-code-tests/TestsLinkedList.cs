f5a382e3d38924c3ef981d6d177546598e4137e2
Luis Rodriguez <lerdeveloper@gmail.com>
2024-09-04 22:57:13 -0400

    Adding new algorith ktoLast.

--- cracking-code-tests/TestsLinkedList.cs
+++ cracking-code-tests/TestsLinkedList.cs
@@ -45,6 +45,44 @@ namespace cracking_code_tests
 
             //Assert.AreEqual(StringsAndArrays.StringsAndArrays.CheckPermutation(s1, s2, true), expected);
         }
+
+        [TestMethod]
+        public void TestKtoLast()
+        {
+            var linkedList = LinkedLists.LinkedLists.ListOf(new int[] { 1, 2, 2, 4, 3, 4 });
+
+            var knode= LinkedLists.LinkedLists.KtoLast(linkedList, 3);
+
+            var after = LinkedLists.LinkedLists.ToList(knode);
+
+            CollectionAssert.AreEqual(new List<int>() {  4, 3, 4 }, after);
+
+
+            /// a second
+            var knode1 = LinkedLists.LinkedLists.KtoLast(linkedList, 5);
+
+            var after1 = LinkedLists.LinkedLists.ToList(knode1);
+
+            CollectionAssert.AreEqual(new List<int>() { 2, 2, 4, 3, 4 }, after1);
+
+
+
+            var knode2 = LinkedLists.LinkedLists.KtoLast(linkedList, 15);
+
+            var after2 = LinkedLists.LinkedLists.ToList(knode2);
+
+            CollectionAssert.AreEqual(new List<int>() { 1, 2, 2, 4, 3, 4 }, after2);
+
+
+
+            var knode3 = LinkedLists.LinkedLists.KtoLast(linkedList, 0);
+
+            var after3 = LinkedLists.LinkedLists.ToList(knode3);
+
+            Assert.AreEqual(0, after3.Count);
+
+
+        }
     }
 
 }
---
+++ cracking-code/LinkedLists/KtoLast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLists
+{
+    public partial class LinkedLists
+    {
+        public static Node<int>? KtoLast(Node<int> head, int number)
+        {
+
+            if (head == null) return null;
+
+            var p1 = head;
+            var p2 = head;
+            int k = number;
+            while (p1 != null)
+            {
+                p1 = p1.Next;
+                k--;
+                if (k < 0) { 
+                  p2 = p2.Next;
+                }
+            }
+
+            return p2;
+        }
+
+    }
+
+}
