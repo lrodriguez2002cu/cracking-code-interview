144287042a7f5cada977fec3a65bd38d33bf2a99
Luis Rodriguez <lerdeveloper@gmail.com>
2024-09-09 11:03:40 -0400

    BreadthFirst implementation

--- cracking-code-tests/TestsTreesAndGraphs.cs
+++ cracking-code-tests/TestsTreesAndGraphs.cs
@@ -6,9 +6,8 @@ namespace cracking_code_tests
     public class TreesAndGraphsTests
     {
         [TestMethod]
-        public void TestGraphBasics()
+        public void TestGraphDepthFirstVisit()
         {
-
             //this graph would be:
             // 1->2->4->3
 
@@ -18,14 +17,45 @@ namespace cracking_code_tests
             var n3 = new Node<int>(3);
             var n4 = new Node<int>(4);
 
-            n1.Children =  new Node<int>[] { n2 };
-            n2.Children =  new Node<int>[] { n4 };
-            n4.Children = new Node<int>[]  { n3 };
-
+            
             var graph = new MyGraph<int>(new Node<int>[] { n1, n2, n3, n4 });
+
+            graph.AddEdge(n1, n2);
+            graph.AddEdge(n2, n4);
+            graph.AddEdge(n4, n3);
 
             TreesAndGraphs.TreesAndGraphs.PrintGraphDepthFirst(graph);
         }
+
+        [TestMethod]
+        public void TestGraphBasicsBreadthFirstVisiting()
+        {
+
+            //this graph would be:
+            // 1->2->4->5
+            // 1->3 
+            // 1->6
+
+            //build some nodes
+            var n1 = new Node<int>(1);
+            var n2 = new Node<int>(2);
+            var n3 = new Node<int>(3);
+            var n4 = new Node<int>(4);
+            var n5= new Node<int>(5);
+            var n6 = new Node<int>(6);
+
+
+            var graph = new MyGraph<int>(new Node<int>[] { n1, n2, n3, n4, n5, n6 });
+
+            graph.AddEdge(n1, n2);
+            graph.AddEdge(n2, n4);
+            graph.AddEdge(n4, n5);
+            graph.AddEdge(n1, n3);
+            graph.AddEdge(n1, n6);
+
+            TreesAndGraphs.TreesAndGraphs.PrintGraphBreadthFirst(graph);
+        }
+
 
 
         [TestMethod]
@@ -46,19 +76,45 @@ namespace cracking_code_tests
 
             var nodes = new Node<int>[] { n1, n2, n3, n4 };
 
-            n1.Children = new Node<int>[] { n2 };
-            n2.Children = new Node<int>[] { n4 };
-            n4.Children = new Node<int>[] { n3 };
+            var graph = new MyGraph<int>(new Node<int>[] { n1, n2, n3, n4 });
+            graph.AddEdge(n1, n2);
+            graph.AddEdge(n2, n4);
+            graph.AddEdge(n4, n3);
+
+            var result = TreesAndGraphs.TreesAndGraphs.RouteBetweenDephFirst(graph, nodes[sn - 1], nodes[tn - 1]);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        [DataRow(1, 2, true)]
+        [DataRow(3, 1, false)]
+
+        public void TestRouteBetween1(int sn, int tn, bool expected)
+        {
+
+            //this graph would be:
+            // 1->2->4->3
+
+            //build some nodes
+            var n1 = new Node<int>(1);
+            var n2 = new Node<int>(2);
+            var n3 = new Node<int>(3);
+            var n4 = new Node<int>(4);
+
+            var nodes = new Node<int>[] { n1, n2, n3, n4 };
 
             var graph = new MyGraph<int>(new Node<int>[] { n1, n2, n3, n4 });
+            graph.AddEdge(n1, n2);
+            graph.AddEdge(n4, n3);
 
-            var result = TreesAndGraphs.TreesAndGraphs.RouteBetween(graph, nodes[sn], nodes[tn]);
+            var result = TreesAndGraphs.TreesAndGraphs.RouteBetweenDephFirst(graph, nodes[sn-1], nodes[tn-1]);
 
             Assert.AreEqual(expected, result);
         }
 
 
-        
+
     }
 
 }
--- cracking-code/TreesAndGraphs/TreesAndGraphs.cs
+++ cracking-code/TreesAndGraphs/TreesAndGraphs.cs
@@ -1,8 +1,11 @@
 
 
 using LinkedLists;
+using System.Collections;
 using System.Diagnostics;
 using System.Numerics;
+using System.Threading.Tasks.Sources;
+using System.Xml.Linq;
 
 namespace TreesAndGraphs
 {
@@ -12,7 +15,7 @@ namespace TreesAndGraphs
 
     public class Node<T> { 
        public T Value { get; set; }
-       public Node<T>[]? Children { get; set; }
+       //public Node<T>[]? Children { get; set; }
     
        public Node(T value) { 
          Value = value; 
@@ -23,9 +26,77 @@ namespace TreesAndGraphs
         }
     }
 
-    public class MyGraph<T>(Node<T>[] nodes)
-    {
-        public Node<T>[] Nodes { get; set; } = nodes;
+    public interface IMyGraph<T> {
+
+        Node<T>[] Nodes { get; set; }
+        Node<T>[] Adjacent(Node<T> n);
+
+        void AddEdge(Node<T> s, Node<T> t);
+    }
+
+    //public class MyGraph<T>(Node<T>[] nodes) : IMyGraph<T>
+    //{
+    //    public Node<T>[] Nodes { get; set; } = nodes;
+
+    //    public Node<T>[] Adjacent(Node<T> n)
+    //    {
+    //        return n.Children
+    //    }
+    //}
+
+    public class MyGraph<T> : IMyGraph<T>{
+
+        readonly bool[,] AdjacencyMatrix;
+        
+        public Node<T>[] Nodes { get ; set ; }
+
+        public MyGraph(Node<T>[] nodes) {
+
+            Nodes = nodes;
+            AdjacencyMatrix = new bool[Nodes.Length, Nodes.Length];
+
+            //for (var i = 0; i< Nodes.Length; i++) {                
+            //    var node = Nodes[i];
+            //    var adjacent = Adjacent(node);
+
+            //    foreach (var chNode in adjacent ?? []) {
+            //        var j = getIndex(chNode);
+            //        AdjacencyMatrix[i, j] = true;
+            //    }
+            //}
+        }
+
+        private int getIndex(Node<T> n){
+            for (var i = 0; i< Nodes.Length; i++) {
+                if (Nodes[i] == n) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public Node<T>[] Adjacent(Node<T> n)
+        {
+            var index = getIndex(n);
+            var result = new List<Node<T>>();
+
+
+            for (var j = 0; j < Nodes.Length; j++) {
+                if (AdjacencyMatrix[index, j]) { 
+                   result.Add(Nodes[j]);
+                }
+            }
+
+            return [.. result];
+        }
+
+        public void AddEdge(Node<T> s, Node<T> t)
+        {
+            var i = getIndex(s);
+            var j = getIndex(t);
+            AdjacencyMatrix[i, j] = true;
+        }
     }
 
     public class Result { 
@@ -34,7 +105,7 @@ namespace TreesAndGraphs
 
     public partial class TreesAndGraphs
     {
-        public static bool RouteBetween<T>(MyGraph<T> graph, Node<T> n1, Node<T> n2) { 
+        public static bool RouteBetweenDephFirst<T>(IMyGraph<T> graph, Node<T> n1, Node<T> n2) { 
            
            //Depth first seems to be appropriate
            List<Node<T>> currentRoute = new List<Node<T>>();
@@ -42,7 +113,7 @@ namespace TreesAndGraphs
            Result r =  new Result();
            HashSet<Node<T>> visiting = new HashSet<Node<T>>();
            
-           DepthFirst(n1, VisitHasRoute(n2, r), visiting);
+           DepthFirst(n1, graph, VisitHasRoute(n2, r), visiting);
            
            return r.Value;
         }
@@ -53,32 +124,34 @@ namespace TreesAndGraphs
             };
         }
 
-        public static void DepthFirst<T>(MyGraph<T> graph, Action<Node<T>> visitFn)
+        public static void DepthFirst<T>(IMyGraph<T> graph, Action<Node<T>> visitFn)
         {
-            HashSet<Node<T>> visiting =  new HashSet<Node<T>>();
+            HashSet<Node<T>> visiting =  new ();
             for (int i = 0; i < graph.Nodes.Length; i++)
             {
                 visiting.Clear();
                 var n = graph.Nodes[i];
                 Debug.WriteLine("");
                 Debug.WriteLine("<<<Node: " + n);
-                DepthFirst(n, visitFn, visiting);
+                DepthFirst(n, graph, visitFn, visiting);
             }
 
         }
 
 
-        public static void DepthFirst<T>(Node<T> n1, Action <Node<T>> visitFn, HashSet<Node<T>> visiting) { 
+        public static void DepthFirst<T>(Node<T> n1, IMyGraph<T> graph, Action <Node<T>> visitFn, HashSet<Node<T>> visiting) { 
             
             if (n1 == null) return;
-            visiting.Add(n1);
 
-            if (n1.Children != null) {
-                for (var i = 0; i < n1.Children.Length; i++)
+            visiting.Add(n1);
+            var adjacent = graph.Adjacent(n1);
+
+            if (adjacent != null) {
+                for (var i = 0; i < adjacent.Length; i++)
                 {
-                    var ch = n1.Children[i];
+                    var ch = adjacent[i];
                     if (!visiting.Contains(ch)) { 
-                       DepthFirst(ch, visitFn, visiting);
+                       DepthFirst(ch, graph, visitFn, visiting);
                     }
                 }
             }
@@ -86,11 +159,58 @@ namespace TreesAndGraphs
         }
 
 
-        public static void PrintGraphDepthFirst<T>(MyGraph<T> myGraph) {
+        public static void PrintGraphDepthFirst<T>(IMyGraph<T> myGraph) {
             
             DepthFirst(myGraph, (Node<T> n) => { 
                 Debug.Write(n.ToString());
             });
+        }
+
+        public static void PrintGraphBreadthFirst<T>(IMyGraph<T> myGraph)
+        {
+
+            BreadthFirst(myGraph, (Node<T> n) => {
+                Debug.Write(n.ToString());
+            });
+        }
+
+
+        public static bool RouteBetweenBreadthFirst<T>(IMyGraph<T> graph, Node<T> n1, Node<T> n2)
+        {
+            return false;
+        }
+
+        public static void BreadthFirst<T>(IMyGraph<T> graph, Action<Node<T>> visitFn) {
+            HashSet<Node<T>> visiting = new();
+            BreadthFirst<T>(graph.Nodes[0], graph, visitFn, visiting);       
+        }
+
+        public static void BreadthFirst<T>(Node<T> n1, IMyGraph<T> graph, Action<Node<T>> visitFn, HashSet<Node<T>> visiting) 
+        {
+
+            Queue<Node<T>> queue = new ();
+            queue.Enqueue(n1);
+            if (n1 == null) return;
+
+            while (queue.Count > 0) {
+
+                var current = queue.Dequeue();
+                var adjacent = graph.Adjacent(current);
+
+                visitFn(current);
+                visiting.Add(current);
+
+                for (var i = 0; i < adjacent.Length; i++)
+                {
+                    var ch = adjacent[i];
+                    if (!visiting.Contains(ch))
+                    {
+                        queue.Enqueue(ch);
+                    }
+                    
+                }
+            }
+            
         }
 
     }
